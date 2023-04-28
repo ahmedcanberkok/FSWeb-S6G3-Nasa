a30980e2ed1d5c69c7452633cc170a655086601f
Canberk Ok <canberk.ok@gmail.com>
2023-04-28 04:11:59 +0300

    Sayfa tamamlandı. ikinci gündeki stylingler yapildi

--- src/App.js
+++ src/App.js
@@ -1,35 +1,56 @@
 import React, { useState, useEffect } from "react";
-import "./App.css";
 import axios from "axios";
+import Title from "./Title";
+import Input from "./Input";
+import Button from "./Button.js";
+import "./App.css";
 
 
-function App() {
-  const [nasaPic, setNasaPic] = useState("");
+
+function App(props) {
+  const [apodData, setApodData] = useState();
+  const [datePicker, setDatePicker] = useState(
+    new Date("2023-02-14").toISOString().slice(0, 10)
+  );
   const [imageDay, setİmageDay] = useState(true);
   const toggleİmage = () => {
     /* ADIM 4 */
     setİmageDay(!imageDay);
   };
-
   useEffect(() => {
 
     axios
-      .get("https://api.nasa.gov/planetary/apod?api_key=5Knl55n9q5i4pM5b6hFcsk8z1oxQ70zSC2GfaEgr")
+      .get("https://api.nasa.gov/planetary/apod", {
+        params: {
+          api_key: "5Knl55n9q5i4pM5b6hFcsk8z1oxQ70zSC2GfaEgr",
+          date: datePicker,
+        },
+      })
+      .then(function (response) {
+        console.log(response);
+        setApodData(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+      .finally(function () { });
+  }, [datePicker]);
 
-      .then(res => setNasaPic(res.data))
-
-      .catch(err => console.error(err));
-    console.log(nasaPic);
-  }, []);
-
-  if (!nasaPic) {
+  if (!apodData) {
     return <div>Loading...</div>;
+  }
+
+  const { data, dateChange, currentDate } = props;
+  function dateChangeHandler(e) {
+    console.log(e.target.value);
+    dateChange(e.target.value);
   }
 
   return (
     <div className="App">
       <header>
-        <h1 > Nasa  </h1>
+        <Title>NASA</Title>
+
         <nav>
           <ul>
             <li>
@@ -38,7 +59,7 @@ function App() {
             <li>
               <a href="#">Hakkımızda</a>
             </li>
-            <li>
+            <li>E
               <a href="#">İletişim</a>
             </li>
           </ul>
@@ -47,18 +68,26 @@ function App() {
       <main>
         <section>
           <p id="h2-üst">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <section>  <Input
+            onChange={(e) => dateChangeHandler(e)}
+            type="date"
+            value={currentDate}
+            name="apodDate"
+          ></Input> </section>
+
           <h2> Astronomy Picture of the Day</h2>
-
           {imageDay && <div id='imageDay' className='pic'></div>}
-          <img
-            style={{ display: imageDay ? "block" : "none" }}
-            src={nasaPic.url}
-            alt={nasaPic.title}
-          />
-
-          <button id='toggleİmage' onClick={toggleİmage}>
-            {imageDay ? "Gizle" : "Göster"}
-          </button>
+          <div className="govde">
+            {apodData.media_type === "image" && (
+              <img style={{ display: imageDay ? "block" : "none" }} src={apodData.hdurl} alt={apodData.title} width="700" height="500" />
+            )}
+            {apodData.media_type === "video" && (
+              <iframe style={{ display: imageDay ? "block" : "none" }} width="520" height="400" src={apodData.url}></iframe>
+            )}
+            <Button id='toggleİmage' onClick={toggleİmage}>
+              {imageDay ? "Gizle" : "Göster"}
+            </Button>
+          </div>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </section>
       </main>
